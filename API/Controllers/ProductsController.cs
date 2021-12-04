f6a66a5027e3e3d60cbd36b260db6af4bf29518f
Mikhail Galas <mgalas2015@gmail.com>
2021-12-03 16:55:49 -0800

    adding some blue prints to product controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,8 +6,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "This will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "single product";
+        }
     }
 }
