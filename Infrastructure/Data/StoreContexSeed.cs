562f015f7f50e1a1b37ec5a62283d3b93632d243
Michael Galas <mgalas2019@hotmail.com>
2022-02-01 22:12:59 -0800

    using jsonserializer to add seed data

--- Infrastructure/Data/StoreContexSeed.cs
+++ Infrastructure/Data/StoreContexSeed.cs
@@ -1,8 +1,34 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
 namespace Infrastructure.Data
 {
     public class StoreContextSeed
     {
-        
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory factory)
+        {
+            try
+            {
+                if(!context.ProductBrands.Any())
+                {
+                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+
+                    foreach (var item in brands)
+                    {
+                        context.ProductBrands.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+        }
     }
 }
 
