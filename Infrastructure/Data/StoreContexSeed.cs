6f066202750012be0aad0855c4aac32674495753
Michael Galas <mgalas2019@hotmail.com>
2022-02-01 22:17:20 -0800

    adding seed data for products and types

--- Infrastructure/Data/StoreContexSeed.cs
+++ Infrastructure/Data/StoreContexSeed.cs
@@ -27,6 +27,34 @@ namespace Infrastructure.Data
 
                     await context.SaveChangesAsync();
                 }
+
+                if(!context.ProductTypes.Any())
+                {
+                    var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+
+                    foreach (var item in types)
+                    {
+                        context.ProductTypes.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if(!context.Products.Any())
+                {
+                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+
+                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+                    foreach (var item in products)
+                    {
+                        context.Products.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
             }
         }
     }
